f750a2b828636a592937fe14985e32ce54d2d032
Mary Alice Maceda <maryalicemaceda@gmail.com>
2023-03-01 05:24:36 -0700

    commit tree e040a09fdf3689ea4aab9ed68497e582f36bed37

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+    return a + b
+}
+function subtract(a, b) {
+    return a - b
+}
+function multiply(a, b) {
+    return a * b
+}
+function divide(a, b) {
+    return a/b
+}
+let number = 10
+function add5() {
+    return (number += 5)
+}
+function divideBy3() {
+    return (number /= 3)
+}
+function increment(n) {
+    return n += 1
+}
+function decrement(n) {
+    return n -= 1
+}
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
